b7ef8bb39d57f9df1b8f6459807e372eb92f975a
wxvirus <1418667580@qq.com>
2023-07-02 23:26:13 +0800

    feat(user-api): 导入导出接口

--- src/main/java/top/wjstar/vue_admin_api/controller/UserController.java
+++ src/main/java/top/wjstar/vue_admin_api/controller/UserController.java
@@ -1,12 +1,20 @@
 package top.wjstar.vue_admin_api.controller;
 
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import top.wjstar.vue_admin_api.entity.User;
 import top.wjstar.vue_admin_api.service.IUserService;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.List;
 
 
@@ -58,6 +66,56 @@ public class UserController {
     @PostMapping("/delete/batch")
     public boolean deleteBatch(@RequestBody List<Integer> ids) {
         return userService.removeByIds(ids);
+    }
+
+    /**
+     * 导出
+     *
+     * @param response
+     * @throws Exception
+     */
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) throws Exception {
+        List<User> list = userService.list();
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+        // 在内存操作，写出到浏览器
+        writer.addHeaderAlias("username", "用户名");
+        writer.addHeaderAlias("password", "密码");
+        writer.addHeaderAlias("nickname", "昵称");
+        writer.addHeaderAlias("email", "邮箱");
+        writer.addHeaderAlias("phone", "电话");
+        writer.addHeaderAlias("address", "地址");
+        writer.addHeaderAlias("createTime", "创建时间");
+        writer.addHeaderAlias("avatar", "头像");
+
+        // 一次性写出 list 数据到 excel 使用默认样式，强制输出标题
+        writer.write(list, true);
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String filename = URLEncoder.encode("用户信息", "UTF-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + filename + ".xlsx");
+
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        out.close();
+        writer.close();
+    }
+
+    /**
+     * 导入
+     *
+     * @param file
+     * @throws Exception
+     */
+    @PostMapping("/import")
+    public Boolean imp(MultipartFile file) throws Exception {
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+        // 弊端：通过 java bean 的方式读取excel 内的对象，但是要求表头必须是英文，跟 java bean 的属性要对应起来
+        List<User> list = reader.readAll(User.class);
+        inputStream.close();
+        System.out.println(list);
+        userService.saveBatch(list);
+        return true;
     }
 }
 
